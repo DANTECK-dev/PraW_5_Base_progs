5c562ac0911c26889d22cbe248504f26989191db
Александр Немеров <nemerov_a@mail.ru>
2021-12-15 17:40:43 +0800

    нееготово

--- PraW_5_Base_progs/Source.cpp
+++ PraW_5_Base_progs/Source.cpp
@@ -1,7 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#define N 10
+#define M 1000
+
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>
+#include <Windows.h>
+
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "rus");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
 
 	/*Напишите программы, решающие задачи, представленные в общих заданиях. 
 	Для решения задач необходимо использовать указатели.
